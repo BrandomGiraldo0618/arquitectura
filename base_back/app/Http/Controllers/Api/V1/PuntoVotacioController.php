93fb6733d0da1b6db72526f64d78fb372464b320
Alexa0710 <alexandracenteno72@gmail.com>
2022-05-26 20:46:48 -0500

    se modifico el controlador de puntos de votacion y el front quedo actualizado.

--- base_back/app/Http/Controllers/Api/V1/PuntoVotacioController.php
+++ base_back/app/Http/Controllers/Api/V1/PuntoVotacioController.php
@@ -6,6 +6,7 @@ use App\Http\Requests\PuntoVotacioRequest;
 use App\Models\PuntoVotacio;
 use App\Http\Controllers\Controller;
 use Illuminate\Http\Request;
+use Illuminate\Http\Response;
 
 class PuntoVotacioController extends Controller
 {
